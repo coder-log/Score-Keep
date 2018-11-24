759a44fe399894dda106f98a679a200603781a17
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-24 08:45:42 +0100

    Excercise exporting and importing a function.

--- server/main.js
+++ server/main.js
@@ -4,5 +4,10 @@ import {
   greetUser
 } from './../imports/utils';
 
+import {
+  add
+} from './../imports/math';
+
 console.log('Log from /server/main.js');
 console.log(greetUser());
+console.log(add(2,4));
