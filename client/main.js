4a643989cc810060161089e3d86d4d46867e4bef
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-25 06:07:48 +0100

    changing title with a variable

--- client/main.js
+++ client/main.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function (){
+  //title -> Account Settings
   let name= 'Coderlog';
-  let jsx = <p>Hello {name}!</p>;
+  let title='Score Keep';
+  let jsx = (
+  <div>
+    {/* Render h1 tag with title var as text */}
+    <h1> {title} </h1>
+    <p>Hello {name}!</p><p>This is a second p.</p>
+  </div>
+  );
   ReactDOM.render((jsx), document.getElementById('app'));
 });
