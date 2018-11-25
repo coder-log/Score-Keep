ec1a1920eee79807275456df109090b7076e949f
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-25 07:00:06 +0100

    Addding a database collection

---
+++ imports/api/players.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+
+new Mongo.Collection('players');
--- imports/utils.js
+++
@@ -1,7 +0,0 @@
-console.log('Log from /imports/utils.js');
-
-export let greetUser = function () {
-  return 'Hello user!'  ;
-};
-
-export let name = 'Coderlog'
