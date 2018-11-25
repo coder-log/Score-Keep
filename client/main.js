c238f82ab630188ffeabe59ae5bc6890fe088e41
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-25 06:35:03 +0100

    rendering players and score dinamically

--- client/main.js
+++ client/main.js
@@ -2,6 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [
+  {
+    _id:1,
+    name: 'Lauren',
+    score: 99
+  },
+  {
+    _id:2,
+    name: 'Mike',
+    score: -1
+  },
+  {
+    _id:3,
+    name: 'Megan',
+    score: -12
+  }
+];
+
+const renderPlayers = function (playersList) {
+
+  return players.map(function (players){
+    return <p key={players._id}> {players.name} has {players.score} points </p>;
+  });
+};
+
 Meteor.startup(function (){
   //title -> Account Settings
   let name= 'Coderlog';
@@ -11,6 +36,7 @@ Meteor.startup(function (){
     {/* Render h1 tag with title var as text */}
     <h1> {title} </h1>
     <p>Hello {name}!</p><p>This is a second p.</p>
+    {renderPlayers()}
   </div>
   );
   ReactDOM.render((jsx), document.getElementById('app'));
