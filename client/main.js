8786b2ca98d2d3c53d6187b16b9478bc817aafab
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-26 07:09:12 +0100

    Rendering MongoDB query in browser

--- client/main.js
+++ client/main.js
@@ -4,27 +4,6 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 import {Tracker} from 'meteor/tracker';
 
-Tracker.autorun( function() {
-console.log('Players List', Players.find().fetch());
-});
-
-const players = [
-  {
-    _id:1,
-    name: 'Lauren',
-    score: 99
-  },
-  {
-    _id:2,
-    name: 'Mike',
-    score: -1
-  },
-  {
-    _id:3,
-    name: 'Megan',
-    score: -12
-  }
-];
 
 const renderPlayers = function (playersList) {
 
@@ -34,21 +13,25 @@ const renderPlayers = function (playersList) {
 };
 
 Meteor.startup(function (){
-
+  // STEP 1. INCLUDE
   //Call tracker.autorun
-  // Create a variable called players -> set equal to fetch query
-  // Render players variable to the screen
-  
-  //title -> Account Settings
-  let name= 'Coderlog';
+  Tracker.autorun( function () {
+
+    //title -> Account Settings
+  const players = Players.find().fetch();
   let title='Score Keep';
   let jsx = (
   <div>
     {/* Render h1 tag with title var as text */}
     <h1> {title} </h1>
-    <p>Hello {name}!</p><p>This is a second p.</p>
+    <p>Hello!</p>
     {renderPlayers(players)}
   </div>
   );
   ReactDOM.render((jsx), document.getElementById('app'));
+
+  });
+  // Create a variable called players -> set equal to fetch query
+  // Render players variable to the screen
+
 });
