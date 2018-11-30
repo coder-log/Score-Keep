9be79f081241ea1b04e489a94eb2ec83904ccf83
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-30 06:07:26 +0100

    Illustrating props binding in react components

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import {Players} from './../api/players'; 
 
-const handleSubmit = (e) => {
-    let playerName = e.target.playerName.value;
-    e.preventDefault();
-    if(playerName) {
-     e.target.playerName.value='';
-      //insert players
-      // Insert new document into players collection
-      Players.insert({
-        name: playerName,
-        score: 0
-    });
-    console.log(Players.find().fetch());
-    }
-  };
+// const handleSubmit = (e) => {
+//     let playerName = e.target.playerName.value;
+//     e.preventDefault();
+//     if(playerName) {
+//      e.target.playerName.value='';
+//       //insert players
+//       // Insert new document into players collection
+//       Players.insert({
+//         name: playerName,
+//         score: 0
+//     });
+//     console.log(Players.find().fetch());
+//     }
+//   };
 
 export default class AddPlayer extends React.Component {
+    handleSubmit(e) {
+        let playerName = e.target.playerName.value;
+        e.preventDefault();
+        if(playerName) {
+         e.target.playerName.value='';
+          //insert players
+          // Insert new document into players collection
+          Players.insert({
+            name: playerName,
+            score: 0;
+        });
+        console.log(Players.find().fetch());
+        }
+    }
     render(){
         return(
             <div>
-                <form onSubmit = {handleSubmit}>
+                <form onSubmit = {this.handleSubmit.bind(this)}>
                     <input type="text" name = "playerName" placeholder ="Player Name" />
                     <button>Add Player</button>
                 </form>
