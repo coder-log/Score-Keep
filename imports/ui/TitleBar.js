74aa77d5c729b26bed13ec2a2e559d1a2d6a7034
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-01 13:13:47 +0100

    Some css styling

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export default class TitleBar extends React.Component{
     render(){
       return (
-        <div>
+        <div className = "title-bar">
             <h1>{this.props.title}</h1>
         </div>
       );
