71fba54933d244b77d519c423343e09125c084a5
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-02 06:56:35 +0100

    Centering PlayerList and AddPlayer

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,8 +9,10 @@ export default class App extends React.Component {
         return(
             <div>
                 <TitleBar title = {this.props.title} />
+                <div className = "wrapper">
                 <PlayerList players = {this.props.players} />
                 <AddPlayer />
+                </div>
             </div>
         );
     }
