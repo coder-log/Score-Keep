7ba07d0d0a5572d15c94defc8bf3b95189ad3cbb
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-03 05:38:20 +0100

    Adding meta tag for responsiveness

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,19 +6,26 @@ export default class Player extends React.Component{
     render() {
         return (
         <div key={this.props.player._id} className="item">
-            <p> 
-            {this.props.player.name} has {this.props.player.score} points
-            </p>
-            <button onClick ={()=>{
-                Players.update({_id: this.props.player._id}, {$inc: {score:1}})
-            }}>+1</button>
-            <button onClick ={()=>{
-                Players.update({_id: this.props.player._id}, {$inc: {score: -1}})
-            }}>-1</button>
-            <button onClick ={()=>{
-                Players.remove({_id:this.props.player._id});
-                alert('deleted');
-            }}>X</button>
+            <div className="player">
+                <div>
+                    <h3 classNAme="player__name">{this.props.player.name}</h3>
+                    <p className ="player__stats"> 
+                    {this.props.player.score} point(s).
+                </p>
+
+                </div>
+                <div className ="player__actions">
+                    <button className="button button--round" onClick ={()=>{
+                        Players.update({_id: this.props.player._id}, {$inc: {score:1}})
+                    }}>+1</button>
+                    <button className="button button--round" onClick ={()=>{
+                        Players.update({_id: this.props.player._id}, {$inc: {score: -1}})
+                    }}>-1</button>
+                    <button className="button button--round" onClick ={()=>{
+                        Players.remove({_id:this.props.player._id})
+                    }}>X</button>
+                </div>
+            </div>
         </div>
     
         );
