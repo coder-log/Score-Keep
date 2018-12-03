62e9a913f2eff741c3a7eac86d7d938dbe8b967e
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-03 08:18:34 +0100

    Adding ranking positions

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
 import {Tracker} from 'meteor/tracker';
+import {Players, calculatePlayersPositions} from './../imports/api/players';
 import App from './../imports/ui/App';
 
 
@@ -11,8 +11,9 @@ import App from './../imports/ui/App';
 Meteor.startup( () => {
   Tracker.autorun( () => {
     const players = Players.find({},{sort:{score:-1}}).fetch();
+    const positionedPlayers = calculatePlayersPositions(players);
     let title='Score Keep';
-  ReactDOM.render(<App title = {title} players = {players}/>, document.getElementById('app'));
+  ReactDOM.render(<App title = {title} players = {positionedPlayers}/>, document.getElementById('app'));
 
   });
 });
