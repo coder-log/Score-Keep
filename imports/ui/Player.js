3383a3d31f38f958b97daf70db812369238cd730
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-04 06:14:38 +0100

    Making dynamic ranking classes

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,29 +4,30 @@ import PropTypes from 'prop-types';
 
 export default class Player extends React.Component{
     render() {
+        let itemClassName = `item item--position-${this.props.player.position}`;
         return (
-        <div key={this.props.player._id} className="item">
-            <div className="player">
-                <div>
-                    <h3 className="player__name">{this.props.player.name}</h3>
-                    <p className ="player__stats"> 
-                    {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s).
-                </p>
+            <div key={this.props.player._id} className="itemClassName">
+                <div className="player">
+                    <div>
+                        <h3 className="player__name">{this.props.player.name}</h3>
+                        <p className ="player__stats"> 
+                        {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s).
+                    </p>
 
-                </div>
-                <div className ="player__actions">
-                    <button className="button button--round" onClick ={()=>{
-                        Players.update({_id: this.props.player._id}, {$inc: {score:1}})
-                    }}>+1</button>
-                    <button className="button button--round" onClick ={()=>{
-                        Players.update({_id: this.props.player._id}, {$inc: {score: -1}})
-                    }}>-1</button>
-                    <button className="button button--round" onClick ={()=>{
-                        Players.remove({_id:this.props.player._id})
-                    }}>X</button>
+                    </div>
+                    <div className ="player__actions">
+                        <button className="button button--round" onClick ={()=>{
+                            Players.update({_id: this.props.player._id}, {$inc: {score:1}})
+                        }}>+1</button>
+                        <button className="button button--round" onClick ={()=>{
+                            Players.update({_id: this.props.player._id}, {$inc: {score: -1}})
+                        }}>-1</button>
+                        <button className="button button--round" onClick ={()=>{
+                            Players.remove({_id:this.props.player._id})
+                        }}>X</button>
+                    </div>
                 </div>
             </div>
-        </div>
     
         );
     }
