c19ae55dda0f794e0ce1ae058ad1ff72542ce017
Xavier <43478582+coder-log@users.noreply.github.com>
2018-12-04 06:16:36 +0100

    Syntax error

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,7 +6,7 @@ export default class Player extends React.Component{
     render() {
         let itemClassName = `item item--position-${this.props.player.position}`;
         return (
-            <div key={this.props.player._id} className="itemClassName">
+            <div key={this.props.player._id} className={itemClassName}>
                 <div className="player">
                     <div>
                         <h3 className="player__name">{this.props.player.name}</h3>
